f983ff276d165f207601c082a461ee7685b70eb7
Rajasssss <103435020+Rajasssss@users.noreply.github.com>
2022-04-29 18:21:54 +0530

    Create CaseMethod.java

---
+++ April28/CaseMethod.java
@@ -0,0 +1,16 @@
+package April28;
+
+public class CaseMethod 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String s1="PaneerMasala";
+		String lcase = s1.toLowerCase();
+		System.out.println(lcase);
+		String ucase = s1.toUpperCase();
+		System.out.println(ucase);
+	}
+
+}
