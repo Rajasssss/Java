bb588161fb88cd39c521f8021dfe8f777bed4e86
Rajasssss <103435020+Rajasssss@users.noreply.github.com>
2022-04-29 18:15:26 +0530

    Create CharAtMethod.java

---
+++ April28/CharAtMethod.java
@@ -0,0 +1,17 @@
+package April28;
+
+public class CharAtMethod 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String obj1="India Maharashtra";
+		char ch=obj1.charAt(0);
+		
+		System.out.println(ch);
+		System.out.println(obj1.charAt(obj1.length()-1));
+		System.out.println(obj1.charAt(16));
+	}
+
+}
