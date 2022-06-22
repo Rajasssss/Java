7c2218a3e8c1ed1ccc9ee56ff24b1baead73f10b
Rajasssss <103435020+Rajasssss@users.noreply.github.com>
2022-06-22 11:53:39 +0530

    Add files via upload

---
+++ lab1606/InvalidForVaccinationException.java
@@ -0,0 +1,12 @@
+package lab1606;
+
+public class InvalidForVaccinationException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidForVaccinationException() {
+		System.out.println("Not eligible for booster dose vaccination ! !");
+	}
+}
